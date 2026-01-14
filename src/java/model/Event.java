611876201ac651aead30dd4c00d98f3d20f09d4f
patohru <patohru2006@gmail.com>
2026-01-14 22:20:33 +0700

    Init project

---
+++ src/java/model/Event.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author patohru
+ */
+public class Event {
+    private String eventID;
+    private String name;
+    private String location;
+    private String date;
+    private float price;
+    private int availableSeats;
+
+    public Event(String eventID, String name, String location, String date, float price, int availableSeats) {
+        this.eventID = eventID;
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.price = price;
+        this.availableSeats = availableSeats;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+    
+    
+    
+}
---
+++ src/java/model/User.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author patohru
+ */
+public class User {
+    private String userID;
+    private String fullName;
+    private String password;
+
+    public User(String userID, String fullName, String password) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
