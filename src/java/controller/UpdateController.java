3e749f27be2dfacff7aa984b0aa2d55134340c74
White2908 <thanh2982006@gmail.com>
2026-01-15 19:54:54 +0700

    Add update and delete fearture

---
+++ src/java/controller/UpdateController.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Event;
+import model.EventDAO;
+
+/**
+ *
+ * @author Sheep
+ */
+public class UpdateController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String id = request.getParameter("id");
+            String name = request.getParameter("name");
+            String location = request.getParameter("location");
+            String date = request.getParameter("date");
+            boolean check = true;
+            
+            if(name.isEmpty()) {
+                request.setAttribute("MSG", "Name of event cannot be empty");
+                check = false;
+            }
+            
+            if(location.isEmpty()) {
+                request.setAttribute("MSG", "Location cannot be empty");
+                check = false;
+            }
+            
+            if(date.isEmpty()) {
+                request.setAttribute("MSG", "Date cannot be empty");
+                check = false;
+            }
+            
+            float price = Float.parseFloat(request.getParameter("price"));
+            int availableSeats = Integer.parseInt(request.getParameter("availableSeats"));
+            Event event = new Event(id, name, location, date, price, availableSeats);
+            EventDAO dao = new EventDAO();
+            if(check) {
+                request.setAttribute("MSG", "Update successfully");
+                dao.update(event);
+            }
+            request.getRequestDispatcher("updateEvent.jsp").forward(request, response);
+         }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
