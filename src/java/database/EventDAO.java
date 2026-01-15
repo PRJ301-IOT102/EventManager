1283db287463c3ca3e86568edefe3f6bc98f0bba
White2908 <thanh2982006@gmail.com>
2026-01-15 22:40:27 +0700

    change EnventDAO location to database

--- src/java/database/EventDAO.java
+++ src/java/database/EventDAO.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package database;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import database.DBUtil;
+import model.Event;
 
 /**
  *
