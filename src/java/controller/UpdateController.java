71b10f66485cf35b4dc873d8d0d3ed9836535d98
White2908 <thanh2982006@gmail.com>
2026-01-15 22:39:21 +0700

    update and delete controller

--- src/java/controller/UpdateController.java
+++ src/java/controller/UpdateController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Event;
-import model.EventDAO;
+import database.EventDAO;
 
 /**
  *
