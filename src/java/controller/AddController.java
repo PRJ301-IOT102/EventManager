eca9fb555459c1decdf2512c9cc64fc73155f98c
thaovann09 <thaovann1405@gmail.com>
2026-01-19 14:48:47 +0700

    move add form from EventList.jsp to add.jsp

--- src/java/controller/AddController.java
+++ src/java/controller/AddController.java
@@ -65,13 +65,7 @@ public class AddController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(AddController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(AddController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        request.getRequestDispatcher("add.jsp").forward(request, response);
     }
 
     /**
